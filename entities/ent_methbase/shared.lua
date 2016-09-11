b2f1473e3cdc3256957687dc18cb6e3099dd394c
sciaschi1 <sciaschi@yahoo.com>
2016-09-11 17:17:24 -0400

    Added folders to set up methrp entities

---
+++ entities/ent_methbase/shared.lua
@@ -0,0 +1,10 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ 
+ENT.PrintName= "Meth Station"
+ENT.Author= "Mister Fox"
+ENT.Contact= "Via steam"
+ENT.Purpose= "Base to make meth"
+ENT.Instructions= "Use ingedients to make meth"
+ENT.Spawnable = true
+ENT.AdminSpawnable = false
