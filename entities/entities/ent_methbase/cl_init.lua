0f9581d77e5412e1bed3c48fbbff17e36fa7e4e5
sciaschi1 <sciaschi@yahoo.com>
2016-09-11 20:06:00 -0400

    Changed some stuff with the meth entity

--- entities/entities/ent_methbase/cl_init.lua
+++ entities/entities/ent_methbase/cl_init.lua
@@ -1,34 +1,21 @@
 include("shared.lua")
 
 function ENT:Draw()
-    -- self.BaseClass.Draw(self) -- Overrides Draw
-    self:DrawEntityOutline( 1.0 ) -- Draw an outline of 1 world unit.
+    //self.BaseClass.Draw(self) -- Overrides Draw
     self:DrawModel() -- Draws Model Client Side
- 
-   	//The amount to display
-	local amount = 10
+	if self:GetPos():Distance(LocalPlayer():GetPos()) < 500 then -- How far people can see printer UI
+			
+		local Pos = self:GetPos()
+		local Ang = self:GetAngles()
 
-	local Pos = self:GetPos()
-	local Ang1 = Angle( 0, 0, 90 )
-	local Ang2 = Angle( 0, 0, 90 )
-
-	Ang1:RotateAroundAxis( Ang1:Right(), self.rotate )
-	Ang2:RotateAroundAxis( Ang2:Right(), self.rotate + 180 )
-
-	//Draws front
-	cam.Start3D2D( Pos + Ang1:Up() * 0, Ang1, 0.2 )
-		draw.DrawText( "$ "..amount, "Default", 0, -50, Color( 0, 255, 0, 255 ), TEXT_ALIGN_CENTER )
-	cam.End3D2D()
-
-	//Draws back
-	cam.Start3D2D( Pos + Ang2:Up() * 0, Ang2, 0.2 )
-		draw.DrawText( "$ "..amount, "Default", 0, -50, Color( 0, 255, 0, 255 ), TEXT_ALIGN_CENTER )
-	cam.End3D2D()
-
-	//Resets the rotation
-	if ( self.rotate > 359 ) then self.rotate = 0 end
-
-	//Rotates
-	self.rotate = self.rotate - ( 100*( self.lasttime-SysTime() ) )
-	self.lasttime = SysTime()
+		Ang:RotateAroundAxis(Ang:Up(), 90)
+		Ang:RotateAroundAxis(Ang:Forward(), 90)
+		
+		cam.Start3D2D(Pos + Ang:Up() * 50 + Ang:Forward() * -15 + Ang:Right() * -40, Ang, 0.11)
+			draw.RoundedBox( 0, 0, 0, 400, 275, Color(20,20,20,255) )
+			surface.DrawOutlinedRect( 0, 0, 275, 275 )
+			surface.DrawOutlinedRect( 1, 1, 273, 273 )
+		cam.End3D2D()
+	end
+	
 end
